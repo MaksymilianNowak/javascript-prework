b4ae5975ca41d51c6c667c1b7fc6a31b62d926c2
Maksymilian Nowak <maksiu102@wp.pl>
2024-06-23 22:07:27 +0200

    Add js/functions.js

--- js/functions.js
+++ js/functions.js
@@ -8,12 +8,27 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-printMessage('Zagrałem ' + 'kamień'+'! Jeśli Twój ruch to papier, to wygrywasz!');
 
-let computerMove = 'kamień';
+let randomNumber = Math.floor(Math.random() * 3+1);
 
-printMessage('Zagrałem '+ computerMove +'! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: '+ randomNumber);
 
-let playerMove = 'papier'
+let computerMove = 'nieznany ruch';
 
-printMessage('Zagrałem ' + playerMove +'! Jeśli Twój ruch to kamień, to wygrywam');
+if (randomNumber == 1){computerMove='kamień';}
+if(randomNumber == 2){computerMove='papier';}
+if(randomNumber == 3){computerMove='nożyce';}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: '+ playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput =='1'){playerMove = 'kamień';}
+if(playerInput =='2'){playerMove = 'papier';}
+if(playerInput =='3'){playerMove = 'nożyce';}
+
+printMessage('Twój ruch to: '+ playerMove);
